431d97dd39478416673b35a155afbd5bf834c191
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-01 08:09:45 +0100

    Init

---
+++ lib/advent20.ex
@@ -0,0 +1,18 @@
+defmodule Advent20 do
+  @moduledoc """
+  Documentation for `Advent20`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Advent20.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent20_test.exs
@@ -0,0 +1,8 @@
+defmodule Advent20Test do
+  use ExUnit.Case
+  doctest Advent20
+
+  test "greets the world" do
+    assert Advent20.hello() == :world
+  end
+end
