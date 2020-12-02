33e2cbaa715b3ef1cc20461a92091638116d83c2
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-02 20:27:00 +0100

    Refactor D1P2

--- lib/advent20/01_expense_report.ex
+++ lib/advent20/01_expense_report.ex
@@ -1,19 +1,40 @@
 defmodule Advent20.ExpenseReport do
   @doc """
-  Find the first match of two numbers in a list of numbers
-  that add up to a given sum
+  Day 1 Part 1: Find two numbers in a set that add up to a sum
   """
-  def find_sum_2(numbers, sum) do
-    number_set = MapSet.new(numbers)
+  def find_sum_2(number_set, sum) do
+    find_sum(number_set, sum)
+  end
 
-    first_number =
-      Enum.find(numbers, fn number ->
-        looking_for = sum - number
-        MapSet.member?(number_set, looking_for)
-      end)
+  @doc """
+  Day 1 Part 2: Find three numbers in a set that add up to a sum
+  """
+  def find_sum_3(number_set, sum) do
+    Enum.reduce_while(number_set, nil, fn number_1, nil ->
+      remaining_sum = sum - number_1
 
-    second_number = sum - first_number
+      number_set
+      |> MapSet.delete(number_1)
+      |> find_sum(remaining_sum)
+      |> case do
+        {number_2, number_3} -> {:halt, {number_1, number_2, number_3}}
+        nil -> {:cont, nil}
+      end
+    end)
+  end
 
-    {first_number, second_number}
+  # Find the first match of two numbers in a list of numbers that add up to a given sum
+  def find_sum(number_set, sum) do
+    Enum.reduce_while(number_set, nil, fn number, nil ->
+      remaining_sum = sum - number
+
+      number_set
+      |> MapSet.delete(number)
+      |> MapSet.member?(remaining_sum)
+      |> case do
+        true -> {:halt, {number, remaining_sum}}
+        false -> {:cont, nil}
+      end
+    end)
   end
 end
--- test/01_expense_report_test.exs
+++ test/01_expense_report_test.exs
@@ -8,25 +8,19 @@ defmodule Advent20.ExpenseReportTest do
             |> File.read!()
             |> String.split("\n", trim: true)
             |> Enum.map(&String.to_integer/1)
+            |> MapSet.new()
 
   test "A: It finds the 2 numbers that sum up to 2020" do
     {number_1, number_2} = ExpenseReport.find_sum_2(@expenses, 2020)
 
     assert number_1 + number_2 == 2020
-    assert number_1 * number_2 == 870331
+    assert number_1 * number_2 == 870_331
   end
 
   test "B: It finds the 3 numbers that sum up to 2020" do
-    results =
-      for expense_1 <- @expenses,
-          expense_2 <- @expenses,
-          expense_3 <- @expenses,
-          expense_1 + expense_2 + expense_3 == 2020,
-          do: {expense_1, expense_2, expense_3}
+    {number_1, number_2, number_3} = ExpenseReport.find_sum_3(@expenses, 2020)
 
-    [{exp_1, exp_2, exp_3} | _tail] = results
-
-    assert exp_1 + exp_2 + exp_3 == 2020
-    assert exp_1 * exp_2 * exp_3 == 283025088
+    assert number_1 + number_2 + number_3 == 2020
+    assert number_1 * number_2 * number_3 == 283_025_088
   end
 end
