1da12609e35832f989fd828ec96676296bef86a6
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-02 10:28:25 +0100

    Refactor D1P1

--- lib/advent20/01_expense_report.ex
+++ lib/advent20/01_expense_report.ex
@@ -1,2 +1,19 @@
 defmodule Advent20.ExpenseReport do
+  @doc """
+  Find the first match of two numbers in a list of numbers
+  that add up to a given sum
+  """
+  def find_sum_2(numbers, sum) do
+    number_set = MapSet.new(numbers)
+
+    first_number =
+      Enum.find(numbers, fn number ->
+        looking_for = sum - number
+        MapSet.member?(number_set, looking_for)
+      end)
+
+    second_number = sum - first_number
+
+    {first_number, second_number}
+  end
 end
--- test/01_expense_report_test.exs
+++ test/01_expense_report_test.exs
@@ -1,6 +1,8 @@
 defmodule Advent20.ExpenseReportTest do
   use ExUnit.Case
 
+  alias Advent20.ExpenseReport
+
   @expenses "01_expense_report.txt"
             |> Path.expand("input_files")
             |> File.read!()
@@ -8,18 +10,10 @@ defmodule Advent20.ExpenseReportTest do
             |> Enum.map(&String.to_integer/1)
 
   test "A: It finds the 2 numbers that sum up to 2020" do
-    results =
-      for expense_1 <- @expenses,
-          expense_2 <- @expenses,
-          expense_1 + expense_2 == 2020,
-          do: {expense_1, expense_2}
+    {number_1, number_2} = ExpenseReport.find_sum_2(@expenses, 2020)
 
-    # We get the result as both {a,b} and {b,a} from the above.
-    # Just proving that with a pattern match:
-    [{exp_1, exp_2}, {exp_2, exp_1}] = results
-
-    assert exp_1 + exp_2 == 2020
-    assert exp_1 * exp_2 == 870331
+    assert number_1 + number_2 == 2020
+    assert number_1 * number_2 == 870331
   end
 
   test "B: It finds the 3 numbers that sum up to 2020" do
