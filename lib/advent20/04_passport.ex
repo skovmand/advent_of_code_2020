eb8e1815f739b93fa0a616b700652d34a2c46618
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-04 07:37:33 +0100

    D4P2

--- lib/advent20/04_passport.ex
+++ lib/advent20/04_passport.ex
@@ -3,6 +3,9 @@ defmodule Advent20.Passport do
   Day 4: Passport Processing
   """
 
+  @doc """
+  Part A
+  """
   def count_valid_passports(passport_input_lists) do
     passport_input_lists
     |> Stream.map(&create_passport/1)
@@ -18,6 +21,90 @@ defmodule Advent20.Passport do
   end
 
   defp valid_passport?(%{} = passport) do
-    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"] |> Enum.all?(&Map.has_key?(passport, &1))
+    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    |> Enum.all?(&Map.has_key?(passport, &1))
+  end
+
+  defmodule Passport do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    embedded_schema do
+      field(:byr, :integer)
+      field(:iyr, :integer)
+      field(:eyr, :integer)
+      field(:hgt, :string)
+      field(:hcl, :string)
+      field(:ecl, :string)
+      field(:pid, :string)
+      field(:cid, :string)
+    end
+
+    def changeset(params) do
+      %__MODULE__{}
+      |> cast(params, [:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid, :cid], empty_values: [])
+      |> validate_required([:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid])
+      |> validate_number(:byr, greater_than_or_equal_to: 1920, less_than_or_equal_to: 2002)
+      |> validate_number(:iyr, greater_than_or_equal_to: 2010, less_than_or_equal_to: 2020)
+      |> validate_number(:eyr, greater_than_or_equal_to: 2020, less_than_or_equal_to: 2030)
+      |> validate_height(:hgt)
+      |> validate_hair_color(:hcl)
+      |> validate_inclusion(:ecl, ~w[amb blu brn gry grn hzl oth])
+      |> validate_length(:pid, is: 9)
+      |> validate_password_is_only_numbers(:pid)
+    end
+
+    defp validate_height(changeset, field) do
+      value = fetch_field!(changeset, field)
+
+      if(not height_valid?(value), do: changeset |> add_error(field, "not valid"), else: changeset)
+    end
+
+    defp height_valid?(input) when is_binary(input) do
+      case Regex.run(~r/(\d+)(cm|in)/, input, capture: :all_but_first) do
+        [height, "cm"] ->
+          int_height = String.to_integer(height)
+          int_height >= 150 and int_height <= 193
+
+        [height, "in"] ->
+          int_height = String.to_integer(height)
+          int_height >= 59 and int_height <= 76
+
+        _ ->
+          false
+      end
+    end
+
+    defp height_valid?(_), do: false
+
+    defp validate_hair_color(changeset, field) do
+      valid? =
+        case fetch_field!(changeset, field) do
+          "#" <> value -> String.length(value) == 6
+          _ -> false
+        end
+
+      if(not valid?, do: changeset |> add_error(field, "not valid"), else: changeset)
+    end
+
+    defp validate_password_is_only_numbers(changeset, field) do
+      valid? =
+        case fetch_field!(changeset, field) do
+          value when is_binary(value) -> Regex.match?(~r/^\d{9}$/, value)
+          _ -> false
+        end
+
+      if(not valid?, do: changeset |> add_error(field, "not valid"), else: changeset)
+    end
+  end
+
+  @doc """
+  Part B
+  """
+  def count_valid_passports_strict(passport_input_lists) do
+    passport_input_lists
+    |> Stream.map(&create_passport/1)
+    |> Stream.map(&Passport.changeset/1)
+    |> Enum.count(& &1.valid?)
   end
 end
--- test/04_passport_test.exs
+++ test/04_passport_test.exs
@@ -7,13 +7,17 @@ defmodule Advent20.PassportTest do
              |> Path.expand("input_files")
              |> File.read!()
              |> String.split("\n")
-             |> Enum.chunk_by(&(&1 == ""))
-             |> Enum.reject(& &1 == [""])
+             |> Stream.chunk_by(&(&1 == ""))
+             |> Stream.reject(&(&1 == [""]))
              |> Enum.map(fn raw_passport ->
                Enum.flat_map(raw_passport, fn line -> String.split(line, " ") end)
              end)
 
   test "A: it counts valid passports" do
     assert Passport.count_valid_passports(@passports) == 170
+  end
+
+  test "B: it counts valid passports with strict rules" do
+    assert Passport.count_valid_passports_strict(@passports) == 103
   end
 end
