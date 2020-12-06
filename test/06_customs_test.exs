c9defc89cb174141ba92d68dc136d69221ccf1a8
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-06 10:23:49 +0100

    Move input file into customs test

--- test/06_customs_test.exs
+++ test/06_customs_test.exs
@@ -3,11 +3,13 @@ defmodule Advent20.CustomsTest do
 
   alias Advent20.Customs
 
+  @input_file "06_customs.txt" |> Path.expand("input_files")
+
   test "A: it sums unique group yes answers" do
-    assert Customs.count_yes_answer_sum_for_all_groups() == 6885
+    assert Customs.count_yes_answer_sum_for_all_groups(@input_file) == 6885
   end
 
   test "B: it sums group answers where everyone agrees" do
-    assert Customs.count_yes_answer_sum_total_agreement_all_groups() == 3550
+    assert Customs.count_yes_answer_sum_total_agreement_all_groups(@input_file) == 3550
   end
 end
