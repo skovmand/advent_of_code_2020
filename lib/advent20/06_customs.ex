9a331d777e466dc5259fde6d63a02cf6352d1710
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-06 09:41:34 +0100

    D6P2

--- lib/advent20/06_customs.ex
+++ lib/advent20/06_customs.ex
@@ -18,4 +18,21 @@ defmodule Advent20.Customs do
     |> Stream.uniq()
     |> Enum.count()
   end
+
+  @doc """
+  Count the sum of yes answers where everyone in the group agrees, for all groups
+  """
+  def count_yes_answer_sum_total_agreement_all_groups(group_answers) do
+    group_answers
+    |> Stream.map(&count_agreeing_answers/1)
+    |> Enum.sum()
+  end
+
+  def count_agreeing_answers(group_answer) do
+    group_answer
+    |> Stream.map(&String.codepoints/1)
+    |> Stream.map(&MapSet.new/1)
+    |> Enum.reduce(&MapSet.intersection/2)
+    |> Enum.count()
+  end
 end
--- test/06_customs_test.exs
+++ test/06_customs_test.exs
@@ -13,4 +13,8 @@ defmodule Advent20.CustomsTest do
   test "A: it sums unique group yes answers" do
     assert Customs.count_yes_answer_sum_for_all_groups(@answers) == 6885
   end
+
+  test "B: it sums group answers where everyone agrees" do
+    assert Customs.count_yes_answer_sum_total_agreement_all_groups(@answers) == 3550
+  end
 end
