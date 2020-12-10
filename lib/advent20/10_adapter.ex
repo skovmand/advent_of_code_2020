840d543c6fd2e66967760384fa0749b348c819ef
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-10 07:42:44 +0100

    Add part 1 comment

--- lib/advent20/10_adapter.ex
+++ lib/advent20/10_adapter.ex
@@ -13,6 +13,9 @@ defmodule Advent20.Adapter do
 
   defp add_outlet_and_computer(jolt_list), do: [0] ++ jolt_list ++ [Enum.max(jolt_list) + 3]
 
+  @doc """
+  What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+  """
   def jolt_differences(input) do
     input
     |> parse()
