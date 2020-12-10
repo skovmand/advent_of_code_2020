e52aea6f04aee2ffbdbe7c742c3a07c030d413ab
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-10 16:06:43 +0100

    Tidy up some more

--- test/08_game_console_test.exs
+++ test/08_game_console_test.exs
@@ -5,21 +5,21 @@ defmodule Advent20.GameConsoleTest do
 
   @input "08_game_console.txt" |> Path.expand("input_files") |> File.read!()
 
+  @unit_test_input """
+  nop +0
+  acc +1
+  jmp +4
+  acc +3
+  jmp -3
+  acc -99
+  acc +1
+  jmp -4
+  acc +6
+  """
+
   describe "1" do
     test "unit test" do
-      input = """
-      nop +0
-      acc +1
-      jmp +4
-      acc +3
-      jmp -3
-      acc -99
-      acc +1
-      jmp -4
-      acc +6
-      """
-
-      assert GameConsole.acc_value_after_first_loop(input) == 5
+      assert GameConsole.acc_value_after_first_loop(@unit_test_input) == 5
     end
 
     test "finds value after first loop" do
@@ -29,19 +29,7 @@ defmodule Advent20.GameConsoleTest do
 
   describe "2" do
     test "unit test" do
-      input = """
-      nop +0
-      acc +1
-      jmp +4
-      acc +3
-      jmp -3
-      acc -99
-      acc +1
-      jmp -4
-      acc +6
-      """
-
-      assert GameConsole.acc_value_at_program_termination(input) == 8
+      assert GameConsole.acc_value_at_program_termination(@unit_test_input) == 8
     end
 
     test "finds the value where the program terminates" do
--- test/09_encoding_test.exs
+++ test/09_encoding_test.exs
@@ -5,32 +5,32 @@ defmodule Advent20.EncodingTest do
 
   @input "09_encoding.txt" |> Path.expand("input_files") |> File.read!()
 
+  @unit_test_input """
+  35
+  20
+  15
+  25
+  47
+  40
+  62
+  55
+  65
+  95
+  102
+  117
+  150
+  182
+  127
+  219
+  299
+  277
+  309
+  576
+  """
+
   describe "part 1" do
     test "unit test" do
-      input = """
-      35
-      20
-      15
-      25
-      47
-      40
-      62
-      55
-      65
-      95
-      102
-      117
-      150
-      182
-      127
-      219
-      299
-      277
-      309
-      576
-      """
-
-      assert Encoding.first_failing_number(input, 5) == 127
+      assert Encoding.first_failing_number(@unit_test_input, 5) == 127
     end
 
     test "puzzle answer" do
@@ -40,30 +40,7 @@ defmodule Advent20.EncodingTest do
 
   describe "part 2" do
     test "unit test" do
-      input = """
-      35
-      20
-      15
-      25
-      47
-      40
-      62
-      55
-      65
-      95
-      102
-      117
-      150
-      182
-      127
-      219
-      299
-      277
-      309
-      576
-      """
-
-      assert Encoding.contigous_set_for_number(input, 127) == 62
+      assert Encoding.contigous_set_for_number(@unit_test_input, 127) == 62
     end
 
     test "puzzle answer" do
