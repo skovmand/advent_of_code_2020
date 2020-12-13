0d5ea42ddf7db6756a740adbddddedee059b6664
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-13 17:31:07 +0100

    D13P2

--- lib/advent20/13_shuttle.ex
+++ lib/advent20/13_shuttle.ex
@@ -3,7 +3,7 @@ defmodule Advent20.Shuttle do
   Day 13: Shuttle Search
   """
 
-  defp parse(input) do
+  defp parse_part_1(input) do
     [earliest_departure, bus_ids] = String.split(input, "\n", trim: true)
 
     %{
@@ -18,7 +18,7 @@ defmodule Advent20.Shuttle do
   """
   def part_1(input) do
     input
-    |> parse()
+    |> parse_part_1()
     |> earliest_bus_id_multiplied()
   end
 
@@ -28,4 +28,96 @@ defmodule Advent20.Shuttle do
     |> Enum.min_by(fn {_id, minutes} -> minutes end)
     |> (fn {id, minutes} -> id * minutes end).()
   end
+
+  defp parse_part_2(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.at(1)
+    |> String.split(",")
+    |> Enum.map(fn
+      "x" -> :x
+      bus_id -> String.to_integer(bus_id)
+    end)
+    |> Enum.with_index()
+    |> Enum.reject(&(elem(&1, 0) == :x))
+  end
+
+  @doc """
+  What is the earliest timestamp such that all of the listed bus IDs depart at
+  offsets matching their positions in the list?
+
+  From the unit test we get these congruences. At t bus 7 leaves, at t+1 bus 13 leaves,
+  at t+4 bus 59 leaves, and so on. So we can make these congruences:
+
+  t≡0 (mod 7)
+  t+1≡0 (mod 13)
+  t+4≡0 (mod 59)
+  t+6≡0 (mod 31)
+  t+7≡0 (mod 19)
+
+  Which is equivalent to the cleaner form (using a remainder of `bus_id - offset`):
+
+  t≡0 (mod 7)
+  t≡12 (mod 13)
+  t≡55 (mod 59)
+  t≡25 (mod 31)
+  t≡12 (mod 19)
+  """
+  def part_2(input) do
+    input
+    |> parse_part_2()
+    |> Enum.map(fn {bus_id, offset} -> {bus_id, bus_id - offset} end)
+    |> gold_star_competition()
+  end
+
+  # Video reference: https://www.youtube.com/watch?v=zIFehsBHB8o
+  defp gold_star_competition(buses) do
+    # The period (N in the chinese theorem), is the product of all the bus ids (which are the modulos)
+    period = buses |> Enum.map(&elem(&1, 0)) |> Enum.reduce(&Kernel.*/2)
+
+    # All pairs of modulos must have a gcd of 1, in other words, be pairwise prime
+    true =
+      buses
+      |> Enum.map(&elem(&1, 0))
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.all?(fn [mod_1, mod_2] -> gcd(mod_1, mod_2) == 1 end)
+
+    # Run the Chinese Remainder algorithm to find the x that solves all our congruences
+    x =
+      buses
+      |> Enum.map(fn {bus_id, remainder} ->
+        # Ni in the chinese remainder theorem is found by dividing
+        # N (the period) with the modulus (the bus id)
+        ni = div(period, bus_id)
+
+        # Next, find the inverse of Ni
+        # if for example Ni is 40 and the modulus is 7, we have to
+        # find the number x for which e.g. 40x≡1 (mod 7)
+        xi = calculate_xi(ni, bus_id)
+
+        ni * xi * remainder
+      end)
+      |> Enum.sum()
+
+    # Uncomment to see the actual numbers:
+    # IO.puts("We need to find an x that will solve:")
+    # buses |> Enum.each(fn {id, offset} -> IO.puts("x≡#{offset} (mod #{id})") end)
+    # IO.puts("Found x: x≡#{x} (mod #{period})")
+
+    rem(x, period)
+  end
+
+  defp calculate_xi(ni, modulus) do
+    # If ni is 40 and modulus is 7, start the search from a base number
+    # in this case 5 can be used because 40x ≡ 1 (mod 7) is congruent with 5x ≡ 1 (mod 7)
+    # (this is because 40 and 5 are congruent mod 7, their remainders are both 5)
+    base = rem(ni, modulus)
+
+    # Then start the search for an x where 5 * x (mod 7) == 1
+    Stream.iterate(0, &(&1 + 1))
+    |> Enum.find(fn value -> rem(base * value, modulus) == 1 end)
+  end
+
+  # Find the greatest common divisor for two numbers
+  defp gcd(a, 0), do: abs(a)
+  defp gcd(a, b), do: gcd(b, rem(a, b))
 end
--- test/13_shuttle_test.exs
+++ test/13_shuttle_test.exs
@@ -19,4 +19,26 @@ defmodule Advent20.ShuttleTest do
       assert Shuttle.part_1(@input) == 115
     end
   end
+
+  describe "2" do
+    defp prepare(bus_ids) do
+      Enum.join(["padding", bus_ids], "\n")
+    end
+
+    test "puzzle examples" do
+      assert "17,x,13,19" |> prepare() |> Shuttle.part_2() == 3417
+      assert "67,7,59,61" |> prepare() |> Shuttle.part_2() == 754_018
+      assert "67,x,7,59,61" |> prepare() |> Shuttle.part_2() == 779_210
+      assert "67,7,x,59,61" |> prepare() |> Shuttle.part_2() == 1_261_476
+      assert "1789,37,47,1889" |> prepare() |> Shuttle.part_2() == 1_202_161_486
+    end
+
+    test "unit test" do
+      assert Shuttle.part_2(@unit_test_input) == 1_068_781
+    end
+
+    test "puzzle answer: id multiplied with waiting time" do
+      assert Shuttle.part_2(@input) == 756_261_495_958_122
+    end
+  end
 end
