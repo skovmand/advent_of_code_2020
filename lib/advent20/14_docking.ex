bcbaa27d151db96b7bc2c81dec513fc3e9860ddd
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-14 19:05:54 +0100

    D14P2

--- lib/advent20/14_docking.ex
+++ lib/advent20/14_docking.ex
@@ -31,9 +31,7 @@ defmodule Advent20.Docking do
     }
 
     program
-    |> Enum.reduce(state, fn instruction, state ->
-      state = exec(instruction, state)
-    end)
+    |> Enum.reduce(state, fn instruction, state -> exec(instruction, state) end)
     |> Map.fetch!(:memory)
     |> Map.values()
     |> Enum.sum()
@@ -41,13 +39,13 @@ defmodule Advent20.Docking do
 
   defp exec({:mask, new_mask}, state), do: %{state | bitmask: new_mask}
 
-  defp exec({:mem, position, value}, state) do
-    binary_string = value |> Integer.to_string(2) |> String.pad_leading(36, "0") |> String.codepoints()
+  defp exec({:mem, address, value}, state) do
+    value_bits = value |> Integer.to_string(2) |> String.pad_leading(36, "0") |> String.codepoints()
 
     masked_value =
       state.bitmask
       |> String.codepoints()
-      |> Enum.zip(binary_string)
+      |> Enum.zip(value_bits)
       |> Enum.map(fn
         {"X", bit} -> bit
         {"0", _} -> "0"
@@ -56,6 +54,53 @@ defmodule Advent20.Docking do
       |> Enum.join()
       |> String.to_integer(2)
 
-    %{state | memory: Map.put(state.memory, position, masked_value)}
+    %{state | memory: Map.put(state.memory, address, masked_value)}
+  end
+
+  @doc """
+  Part 2: Execute the initialization program using an emulator for a version 2
+  decoder chip. What is the sum of all values left in memory after it completes?
+  """
+  def part_2(input) do
+    program = input |> parse()
+
+    state = %{
+      bitmask: nil,
+      memory: %{}
+    }
+
+    program
+    |> Enum.reduce(state, fn instruction, state -> exec_v2(instruction, state) end)
+    |> Map.fetch!(:memory)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  defp exec_v2({:mask, new_mask}, state), do: %{state | bitmask: new_mask}
+
+  defp exec_v2({:mem, address, value}, state) do
+    address_bits = address |> Integer.to_string(2) |> String.pad_leading(36, "0") |> String.codepoints()
+
+    state.bitmask
+    |> String.codepoints()
+    |> Enum.zip(address_bits)
+    |> Enum.map(fn
+      {"0", bit} -> bit
+      {"1", _} -> "1"
+      {"X", _} -> :floating
+    end)
+    |> Enum.reduce([""], fn
+      :floating, acc ->
+        acc_1 = acc |> Enum.map(&(&1 <> "0"))
+        acc_2 = acc |> Enum.map(&(&1 <> "1"))
+        acc_1 ++ acc_2
+
+      char, acc ->
+        Enum.map(acc, &(&1 <> char))
+    end)
+    |> Enum.map(&String.to_integer(&1, 2))
+    |> Enum.reduce(state, fn address, state ->
+      %{state | memory: Map.put(state.memory, address, value)}
+    end)
   end
 end
--- test/14_docking_test.exs
+++ test/14_docking_test.exs
@@ -5,20 +5,37 @@ defmodule Advent20.DockingTest do
 
   @input "14_docking.txt" |> Path.expand("input_files") |> File.read!()
 
-  @unit_test_input """
-  mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-  mem[8] = 11
-  mem[7] = 101
-  mem[8] = 0
-  """
-
   describe "1" do
     test "unit test" do
-      assert Docking.part_1(@unit_test_input) == 165
+      program = """
+      mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+      mem[8] = 11
+      mem[7] = 101
+      mem[8] = 0
+      """
+
+      assert Docking.part_1(program) == 165
     end
 
     test "puzzle answer: sum of all values in memory" do
       assert Docking.part_1(@input) == 8_570_568_288_597
+    end
+  end
+
+  describe "2" do
+    test "unit test" do
+      program = """
+      mask = 000000000000000000000000000000X1001X
+      mem[42] = 100
+      mask = 00000000000000000000000000000000X0XX
+      mem[26] = 1
+      """
+
+      assert Docking.part_2(program) == 208
+    end
+
+    test "puzzle answer: sum of all values in memory" do
+      assert Docking.part_2(@input) == 3_289_441_921_203
     end
   end
 end
