2912753ea9f846432a73202ec91572966dee6f95
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-18 21:15:12 +0100

    D18P2

--- lib/advent20/18_operation_order.ex
+++ lib/advent20/18_operation_order.ex
@@ -1,5 +1,21 @@
 defmodule Advent20.OperationOrder do
   @moduledoc """
+  Day 18: Operation Order
+
+  Phew this day was hard. I am not used to parsing. I read up on algorithms for parsing math expressions
+  and read about the difference between infix and postfix notation. Infix is e.g. 2+4*(3+4) and the same
+  in postfix would be written as 2434+*+ which at first seems really weird.
+
+  However! The postfix notation is really easy to evaluate on a computer. It can be done using only a single
+  stack and a recursive function (as done in this module at the bottom in `evaluate_postfix/1`).
+
+  So. This D18 consists of a infix to postfix converter which takes custom operator precedences for only the
+  + and * operators. It converts all input equations to postfix using these precedences and then evaluates
+  the postfix equations.
+
+  The D18 puzzles is actually our normal math, but with custom operator precedences.
+  In part 1 the precedences are the same for + and * (I have set both to 1)
+  In part 2 the + operator takes precedence over *, so the precedence is 2 for + and 1 for -
   """
 
   def parse(input) do
@@ -9,10 +25,18 @@ defmodule Advent20.OperationOrder do
   end
 
   def part_1(input) do
+    calculate(input, %{*: 1, +: 1})
+  end
+
+  def part_2(input) do
+    calculate(input, %{*: 1, +: 2})
+  end
+
+  def calculate(input, operator_precedences) do
     input
     |> parse()
     |> Enum.map(&tokenize/1)
-    |> Enum.map(&to_postfix/1)
+    |> Enum.map(&to_postfix(&1, operator_precedences))
     |> Enum.map(&evaluate_postfix/1)
     |> Enum.sum()
   end
@@ -30,56 +54,55 @@ defmodule Advent20.OperationOrder do
     end)
   end
 
-  @operator_precedence %{*: 1, +: 1}
-
-  def to_postfix(tokens) do
-    do_parse_rpe(tokens, [], [])
+  def to_postfix(tokens, precedence) do
+    do_parse_rpe(tokens, [], [], precedence)
   end
 
   # Handle a number by pushing it into postfix
-  defp do_parse_rpe([number | equation], postfix, opstack) when is_integer(number) do
-    do_parse_rpe(equation, [number | postfix], opstack)
+  defp do_parse_rpe([number | equation], postfix, opstack, precedence) when is_integer(number) do
+    do_parse_rpe(equation, [number | postfix], opstack, precedence)
   end
 
   # Encountering a start parenthesis, just push it to the opstack
-  defp do_parse_rpe([:start_parens | equation], postfix, opstack) do
-    do_parse_rpe(equation, postfix, [:start_parens | opstack])
+  defp do_parse_rpe([:start_parens | equation], postfix, opstack, precedence) do
+    do_parse_rpe(equation, postfix, [:start_parens | opstack], precedence)
   end
 
   # End parenthesis, pop from the opstack onto the postfix until the start parens is encountered
   # Neither start or end parens are added to the postfix
-  defp do_parse_rpe([:end_parens | equation], postfix, opstack) do
+  defp do_parse_rpe([:end_parens | equation], postfix, opstack, precedence) do
     {remaning_parens_ops, [:start_parens | new_opstack]} = Enum.split_while(opstack, fn x -> x != :start_parens end)
-    do_parse_rpe(equation, Enum.reverse(remaning_parens_ops) ++ postfix, new_opstack)
+    do_parse_rpe(equation, Enum.reverse(remaning_parens_ops) ++ postfix, new_opstack, precedence)
   end
 
   # Handle operations
   # If the first element in the opstack is a parens, just push the operator to the opstack
-  defp do_parse_rpe([operator | equation], postfix, [:start_parens | _] = opstack) when operator in [:*, :+] do
-    do_parse_rpe(equation, postfix, [operator | opstack])
+  defp do_parse_rpe([operator | equation], postfix, [:start_parens | _] = opstack, precedence)
+       when operator in [:*, :+] do
+    do_parse_rpe(equation, postfix, [operator | opstack], precedence)
   end
 
   # The same if the opstack is empty, just push the operator
-  defp do_parse_rpe([operator | equation], postfix, [] = opstack) when operator in [:*, :+] do
-    do_parse_rpe(equation, postfix, [operator | opstack])
+  defp do_parse_rpe([operator | equation], postfix, [] = opstack, precedence) when operator in [:*, :+] do
+    do_parse_rpe(equation, postfix, [operator | opstack], precedence)
   end
 
   # If the first operator in the opstack has a precedence
-  defp do_parse_rpe([operator | equation], postfix, opstack) when operator in [:*, :+] do
-    operator_precedence = Map.fetch!(@operator_precedence, operator)
-    first_opstack_element_precedence = Map.fetch!(@operator_precedence, List.first(opstack))
+  defp do_parse_rpe([operator | equation], postfix, opstack, precedence) when operator in [:*, :+] do
+    operator_precedence = Map.fetch!(precedence, operator)
+    first_opstack_element_precedence = Map.fetch!(precedence, List.first(opstack))
 
-    # If the operator precendence is greater than or equal to the next operator in the opstack, just push it to the opstack
+    # If the operator precedence is greater than or equal to the next operator in the opstack, just push it to the opstack
     # Otherwise, pop the opstack operator into the postfix and put the operator into the opstack
     if operator_precedence > first_opstack_element_precedence do
-      do_parse_rpe(equation, postfix, [operator | opstack])
+      do_parse_rpe(equation, postfix, [operator | opstack], precedence)
     else
       [opstack_operator | opstack_rest] = opstack
-      do_parse_rpe(equation, [opstack_operator | postfix], [operator | opstack_rest])
+      do_parse_rpe(equation, [opstack_operator | postfix], [operator | opstack_rest], precedence)
     end
   end
 
-  defp do_parse_rpe([], postfix, []), do: postfix |> Enum.reverse()
+  defp do_parse_rpe([], postfix, [], _), do: postfix |> Enum.reverse()
 
   defp evaluate_postfix(postfix) do
     do_evaluate_postfix(postfix, [])
--- test/18_operation_order_test.exs
+++ test/18_operation_order_test.exs
@@ -7,10 +7,19 @@ defmodule Advent20.OperationOrderTest do
 
   describe "1" do
     test "postfix test" do
-      assert "2*3+(4*5)" |> parse() |> List.first() |> tokenize() |> to_postfix() |> Enum.join() == "23*45*+"
+      to_postfix_string = fn input, precedences ->
+        input
+        |> parse()
+        |> List.first()
+        |> tokenize()
+        |> to_postfix(precedences)
+        |> Enum.join()
+      end
 
-      assert "5+(8*3+9+3*4*3)" |> parse() |> List.first() |> tokenize() |> to_postfix() |> Enum.join() ==
-               "583*9+3+4*3*+"
+      assert to_postfix_string.("2*3+(4*5)", %{*: 1, +: 1}) == "23*45*+"
+      assert to_postfix_string.("2*3+(4*5)", %{*: 2, +: 1}) == "23*45*+"
+      assert to_postfix_string.("5+(8*3+9+3*4*3)", %{*: 1, +: 1}) == "583*9+3+4*3*+"
+      assert to_postfix_string.("5+(8*3+9+3*4*3)", %{*: 2, +: 1}) == "583*9+34*3*++"
     end
 
     test "unit tests" do
@@ -21,7 +30,21 @@ defmodule Advent20.OperationOrderTest do
     end
 
     test "puzzle answer: sum of the resulting values" do
-      assert part_1(@input) == 11297104473091
+      assert part_1(@input) == 11_297_104_473_091
+    end
+  end
+
+  describe "2" do
+    test "unit tests" do
+      assert part_2("1 + (2 * 3) + (4 * (5 + 6))") == 51
+      assert part_2("2 * 3 + (4 * 5)") == 46
+      assert part_2("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+      assert part_2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669_060
+      assert part_2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
+    end
+
+    test "puzzle answer: sum of the resulting values" do
+      assert part_2(@input) == 185_348_874_183_674
     end
   end
 end
