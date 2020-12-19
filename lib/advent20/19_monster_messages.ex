c135f988987a24b7453bd10f703c54e11606c800
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-19 20:33:08 +0100

    D19P2 WIP

--- lib/advent20/19_monster_messages.ex
+++ lib/advent20/19_monster_messages.ex
@@ -7,34 +7,37 @@ defmodule Advent20.MonsterMessages do
     [rules_raw, messages_raw] = String.split(input, "\n\n", trim: true)
 
     regexes = [
-      ~r/(\d+): (\d+)$/,
-      ~r/(\d+): (\d+) (\d+) (\|) (\d+) (\d+)$/,
-      ~r/(\d+): (\d+) (\|) (\d+)$/,
-      ~r/(\d+): (\d+) (\d+) (\d+)$/,
-      ~r/(\d+): (\d+) (\d+)$/,
-      ~r/(\d+): "([a-z])"$/
+      ~r/^(\d+): (\d+)$/,
+      ~r/^(\d+): (\d+) (\|) (\d+)$/,
+      ~r/^(\d+): (\d+) (\|) (\d+) (\d+)$/,
+      ~r/^(\d+): (\d+) (\d+) (\|) (\d+) (\d+)$/,
+      ~r/^(\d+): (\d+) (\d+) (\|) (\d+) (\d+) (\d+)$/,
+      ~r/^(\d+): (\d+) (\d+) (\d+)$/,
+      ~r/^(\d+): (\d+) (\d+)$/,
+      ~r/^(\d+): "([a-z])"$/
     ]
 
     rules =
       rules_raw
-      |> String.split("\n")
+      |> String.split("\n", trim: true)
       |> Enum.map(fn string ->
         Enum.find_value(regexes, &Regex.run(&1, string, capture: :all_but_first))
       end)
       |> Enum.map(fn [number | tail] -> {number, tail} end)
       |> Enum.into(%{}, fn {number, signature} -> {number, rule_match_function(signature)} end)
 
-    {rules, messages_raw |> String.split("\n", trim: true)}
+    messages =
+      messages_raw
+      |> String.split("\n", trim: true)
+
+    {rules, messages}
   end
 
   # Generate a function that matches the a or b literals, e.g. ('22: "a"')
   defp rule_match_function([char]) when char in ["a", "b"] do
-    fn [input_char | tail], _rules ->
-      if input_char == char do
-        {:ok, tail}
-      else
-        :error
-      end
+    fn
+      [input_char | tail], _rules -> if(input_char == char, do: {:ok, tail}, else: :error)
+      [], _rules -> :underflow
     end
   end
 
@@ -68,6 +71,7 @@ defmodule Advent20.MonsterMessages do
       case matcher1.(list, rules) do
         {:ok, rest} -> {:ok, rest}
         :error -> matcher2.(list, rules)
+        :underflow -> :underflow
       end
     end
   end
@@ -83,6 +87,29 @@ defmodule Advent20.MonsterMessages do
            {:ok, rest} <- matcher2.(rest, rules),
            {:ok, rest} <- matcher3.(rest, rules) do
         {:ok, rest}
+      end
+    end
+  end
+
+  # Generate a matcher for or-style rule matchers (e.g. "0: 1 | 4 5")
+  defp rule_match_function([rule1, "|", rule2, rule3]) do
+    fn list, rules ->
+      matcher1 = Map.fetch!(rules, rule1)
+      matcher2 = Map.fetch!(rules, rule2)
+      matcher3 = Map.fetch!(rules, rule3)
+
+      case matcher1.(list, rules) do
+        {:ok, rest} ->
+          {:ok, rest}
+
+        :error ->
+          with {:ok, rest} <- matcher2.(list, rules),
+               {:ok, rest} <- matcher3.(rest, rules) do
+            {:ok, rest}
+          end
+
+        :underflow ->
+          :underflow
       end
     end
   end
@@ -104,6 +131,35 @@ defmodule Advent20.MonsterMessages do
                {:ok, rest} <- matcher4.(rest, rules) do
             {:ok, rest}
           end
+
+        :underflow ->
+          :underflow
+      end
+    end
+  end
+
+  # Generate a matcher for or-style rule matchers (e.g. "0: 1 4 | 4 5 7")
+  defp rule_match_function([rule1, rule2, "|", rule3, rule4, rule5]) do
+    fn list, rules ->
+      matcher1 = Map.fetch!(rules, rule1)
+      matcher2 = Map.fetch!(rules, rule2)
+      matcher3 = Map.fetch!(rules, rule3)
+      matcher4 = Map.fetch!(rules, rule4)
+      matcher5 = Map.fetch!(rules, rule5)
+
+      with {:ok, rest} <- matcher1.(list, rules),
+           {:ok, rest} <- matcher2.(rest, rules) do
+        {:ok, rest}
+      else
+        :error ->
+          with {:ok, rest} <- matcher3.(list, rules),
+               {:ok, rest} <- matcher4.(rest, rules),
+               {:ok, rest} <- matcher5.(rest, rules) do
+            {:ok, rest}
+          end
+
+        :underflow ->
+          :underflow
       end
     end
   end
@@ -129,6 +185,10 @@ defmodule Advent20.MonsterMessages do
       # The validation failed
       :error ->
         false
+
+      # We ran out of string
+      :underflow ->
+        true
     end
   end
 end
