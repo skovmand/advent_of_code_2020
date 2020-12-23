3a6df768f4cf58dc6ecdcbe5c139967fb5ea1f29
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-23 14:53:11 +0100

    D23P2

--- lib/advent20/23_crab_cups.ex
+++ lib/advent20/23_crab_cups.ex
@@ -6,6 +6,7 @@ defmodule Advent20.CrabCups do
   defp parse(input) do
     String.trim(input)
     |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
   end
 
   @doc """
@@ -21,75 +22,121 @@ defmodule Advent20.CrabCups do
     |> labels()
   end
 
-  defp first_state(circle) do
+  @doc """
+  Part 2: Determine which two cups will end up immediately clockwise of cup 1.
+  What do you get if you multiply their labels together?
+  """
+  def part_2(input) do
+    input
+    |> parse()
+    |> fill()
+    |> first_state()
+    |> Stream.iterate(&step/1)
+    |> Enum.find(&(&1.move == 10_000_001))
+    |> two_cups()
+  end
+
+  defp first_state(input_cups) do
+    last_cup = List.last(input_cups)
+
     %{
-      circle: circle,
+      ring: ring(input_cups),
       move: 1,
-      picked_cup: List.last(circle)
+      picked_cup: last_cup,
+      max_label: Enum.max(input_cups)
     }
   end
 
-  defp step(%{circle: circle, move: move, picked_cup: prev_picked_cup}) do
-    current_cup = current_cup(circle, prev_picked_cup)
-    pickup = pickup_cups(circle, current_cup)
-    destination_cup = destination_cup(circle, current_cup, pickup)
+  # Fill the list with numbers to 1_000_000
+  defp fill(input_cups) do
+    from = Enum.max(input_cups) + 1
+    extra_numbers = from..1_000_000 |> Enum.to_list()
+
+    input_cups ++ extra_numbers
+  end
+
+  defp ring([first_cup | _] = input_cups) do
+    input_cups
+    |> Kernel.++([first_cup])
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Enum.into(%{}, fn [current, next] -> {current, next} end)
+  end
+
+  defp step(%{ring: ring, move: move, picked_cup: prev_picked_cup, max_label: max_label}) do
+    current_cup = Map.fetch!(ring, prev_picked_cup)
+    pickup = pickup_cups(ring, current_cup)
+    destination_cup = destination_cup(current_cup, pickup, max_label)
+    updated_ring = place_pickup(ring, current_cup, pickup, destination_cup)
 
     %{
+      max_label: max_label,
       move: move + 1,
-      circle: place_pickup(circle, pickup, destination_cup),
+      ring: updated_ring,
       picked_cup: current_cup
     }
   end
 
-  # Get the current cup. This is the cup next to the previous cup, clockwise.
-  defp current_cup(circle, prev_picked_cup) do
-    Stream.cycle(circle)
-    |> Stream.drop_while(&(&1 != prev_picked_cup))
-    |> Stream.drop(1)
+  # Pick up the 3 cups after the current cup from the ring
+  defp pickup_cups(ring, current_cup) do
+    n_plus_1 = Map.fetch!(ring, current_cup)
+    n_plus_2 = Map.fetch!(ring, n_plus_1)
+    n_plus_3 = Map.fetch!(ring, n_plus_2)
+
+    [n_plus_1, n_plus_2, n_plus_3]
+  end
+
+  # try cups from cup_label and decreasing by one, wrap when cup_label < 1
+  defp destination_cup(current_cup, pickup, max) do
+    current_cup
+    |> Stream.iterate(fn cup_label ->
+      cup_label
+      |> Kernel.-(1)
+      |> case do
+        number when number < 1 -> max
+        number -> number
+      end
+    end)
+    |> Stream.reject(&(&1 == current_cup))
+    |> Stream.reject(&(&1 in pickup))
     |> Enum.take(1)
     |> hd()
   end
 
-  # Pick up the 3 cups after the current cup. Using stream for the fun of it.
-  defp pickup_cups(circle, current_cup) do
-    Stream.cycle(circle)
-    |> Stream.drop_while(&(&1 != current_cup))
-    |> Stream.drop(1)
-    |> Enum.take(3)
-  end
+  # place the pickup in the ring by updating 3 keys
+  # first, update destination_cup to point at first pickup card
+  # then update last pickup card to point to the card the destination_cup pointed to before
+  # lastly point the current cup to the card that pickup_3 pointed to before
+  defp place_pickup(ring, current_cup, [pickup_1, _, pickup_3], destination_cup) do
+    next_cup = Map.fetch!(ring, pickup_3)
+    previous_dest_cup_next = Map.fetch!(ring, destination_cup)
 
-  # Remove the pickup from the circle, then sort in descending order, jump to the
-  # current cup and take the next value
-  defp destination_cup(circle, current_cup, pickup) do
-    circle
-    |> Enum.reject(&(&1 in pickup))
-    |> Enum.sort(&Kernel.>=/2)
-    |> Stream.cycle()
-    |> Stream.drop_while(&(&1 != current_cup))
-    |> Stream.drop(1)
-    |> Enum.take(1)
-    |> hd()
-  end
-
-  # Place the pickup right after the destination cup
-  defp place_pickup(circle, pickup, destination_cup) do
-    circle_without_pickup = Enum.reject(circle, &(&1 in pickup))
-
-    dest_index =
-      Enum.find_index(circle_without_pickup, &(&1 == destination_cup))
-      |> Kernel.+(1)
-
-    circle_without_pickup
-    |> List.insert_at(dest_index, pickup)
-    |> List.flatten()
+    ring
+    |> Map.put(destination_cup, pickup_1)
+    |> Map.put(pickup_3, previous_dest_cup_next)
+    |> Map.put(current_cup, next_cup)
   end
 
   # Get the final labels
-  defp labels(%{circle: circle}) do
-    Stream.cycle(circle)
-    |> Stream.drop_while(&(&1 != "1"))
-    |> Stream.drop(1)
-    |> Enum.take(8)
+  defp labels(%{ring: ring}) do
+    c1 = Map.fetch!(ring, 1)
+    c2 = Map.fetch!(ring, c1)
+    c3 = Map.fetch!(ring, c2)
+    c4 = Map.fetch!(ring, c3)
+    c5 = Map.fetch!(ring, c4)
+    c6 = Map.fetch!(ring, c5)
+    c7 = Map.fetch!(ring, c6)
+    c8 = Map.fetch!(ring, c7)
+
+    [c1, c2, c3, c4, c5, c6, c7, c8]
+    |> Enum.map(&Integer.to_string/1)
     |> Enum.join()
+  end
+
+  # Get the final labels
+  defp two_cups(%{ring: ring}) do
+    cup_1 = Map.fetch!(ring, 1)
+    cup_2 = Map.fetch!(ring, cup_1)
+
+    cup_1 * cup_2
   end
 end
--- test/23_crab_cups_test.exs
+++ test/23_crab_cups_test.exs
@@ -14,4 +14,16 @@ defmodule Advent20.CrabCupsTest do
       assert CrabCups.part_1(@input) == "47598263"
     end
   end
+
+  describe "2" do
+    @tag timeout: 120_000
+    test "unit test" do
+      assert CrabCups.part_2("389125467") == 149_245_887_792
+    end
+
+    @tag timeout: 120_000
+    test "puzzle answer" do
+      assert CrabCups.part_2(@input) == 248_009_574_232
+    end
+  end
 end
