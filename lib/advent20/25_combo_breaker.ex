e9f9e686e35d8d4a18dd2d25f4a92e80f69fce19
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-27 12:45:18 +0100

    Optimize D25 with two separate transforms

--- lib/advent20/25_combo_breaker.ex
+++ lib/advent20/25_combo_breaker.ex
@@ -3,66 +3,26 @@ defmodule Advent20.ComboBreaker do
   --- Day 25: Combo Breaker ---
   """
 
-  defmodule Cache do
-    use Agent
-
-    def start_link() do
-      Agent.start_link(fn -> %{} end, name: __MODULE__)
-    end
-
-    def get(key) do
-      Agent.get(__MODULE__, &Map.fetch(&1, key))
-    end
-
-    def put(key, value) do
-      Agent.update(__MODULE__, &Map.put(&1, key, value))
-    end
-  end
-
   defp parse(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 
-  # Blarh, it turns out Elixir has no :math.pow/3 as Pythons pow/3 for doing
-  # base_number to power exp modulo mod.
-  #
-  # From python 3.9 docs: pow(base, exp, [mod]): Return base to the power exp;
-  # if mod is present, return base to the power exp, modulo mod (computed more
-  # efficiently than pow(base, exp) % mod)
-  #
-  # I have made a quickfix to this by simply making a cache
   def part_1(input) do
-    Cache.start_link()
     pubkeys = parse(input)
 
-    {loop_size_1, pubkey_1} =
-      Stream.iterate(1, &(&1 + 1))
-      |> Stream.map(fn loop_size -> {loop_size, transform_subject_number(7, loop_size)} end)
-      |> Enum.find(fn {_loop_size, pubkey} -> pubkey in pubkeys end)
+    # Find the loop size of the first matching public key using subject_number 7
+    {pubkey_1, loop_size_1} =
+      Stream.iterate(1, &rem(&1 * 7, 20_201_227))
+      |> Stream.with_index()
+      |> Enum.find(fn {pubkey, _loop_size} -> pubkey in pubkeys end)
 
+    # Get the other public key
     [other_pubkey] = Enum.reject(pubkeys, &(&1 == pubkey_1))
 
-    transform_subject_number(other_pubkey, loop_size_1)
-  end
-
-  def transform_subject_number(subject_number, loop_size) do
-    result =
-      case Cache.get({subject_number, loop_size - 1}) do
-        {:ok, prev_result} ->
-          rem(prev_result * subject_number, 20_201_227)
-
-        :error ->
-          calculate_transform_subject_number(subject_number, loop_size)
-      end
-
-    Cache.put({subject_number, loop_size}, result)
-    result
-  end
-
-  def calculate_transform_subject_number(subject_number, loop_size) do
-    Stream.iterate(1, &rem(&1 * subject_number, 20_201_227))
-    |> Enum.at(loop_size)
+    # Then, run the transform against the other pubkey, using our the known loop size
+    Stream.iterate(1, &rem(&1 * other_pubkey, 20_201_227))
+    |> Enum.at(loop_size_1)
   end
 end
--- test/25_combo_breaker_test.exs
+++ test/25_combo_breaker_test.exs
@@ -6,22 +6,6 @@ defmodule Advent20.ComboBreakerTest do
   @input "25_combo_breaker.txt" |> Path.expand("input_files") |> File.read!()
 
   describe "1" do
-    test "transform_subject_number" do
-      ComboBreaker.Cache.start_link()
-
-      # Calculate public key for the card
-      assert Advent20.ComboBreaker.transform_subject_number(7, 8) == 5_764_801
-
-      # Calculate public key for the door
-      assert Advent20.ComboBreaker.transform_subject_number(7, 11) == 17_807_724
-
-      # Use cards public key and door loop size to get encryption key
-      assert Advent20.ComboBreaker.transform_subject_number(5_764_801, 11) == 14_897_079
-
-      # Use cards public key and door loop size to get encryption key
-      assert Advent20.ComboBreaker.transform_subject_number(17_807_724, 8) == 14_897_079
-    end
-
     test "unit test" do
       input = """
       5764801
@@ -31,7 +15,6 @@ defmodule Advent20.ComboBreakerTest do
       assert ComboBreaker.part_1(input) == 14_897_079
     end
 
-    @tag timeout: 120_000
     # The test runs in ~40 sec on my machine, but not on GitHub Actions, which needs a longer timeout
     test "puzzle answer" do
       assert ComboBreaker.part_1(@input) == 9_177_528
