132348cef13556e4eeb9e450f75ed309e586dc16
Niels Kristian Skovmand <niels@skovmand.org>
2024-11-17 10:19:44 +0100

    Add benchmark

---
+++ benchmarks/01_expense_report.exs
@@ -0,0 +1,6 @@
+input = Path.expand("01_expense_report.txt", "input_files") |> File.read!()
+
+Benchee.run(%{
+  "part1" => fn -> Advent20.ExpenseReport.find_sum_of_2_numbers(input) end,
+  "part2" => fn -> Advent20.ExpenseReport.find_sum_of_3_numbers(input) end
+})
